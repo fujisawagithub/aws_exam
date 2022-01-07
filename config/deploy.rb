7516c9b9e027df5a73da41d92d5cc90ea8bb6d09
yuki fujisawa <fujisawa.yuki@outlook.com>
2022-01-07 10:46:57 +0900

    Fix アプリ名とブランチ名修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application, 'achieve'
 # （xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
 set :repo_url, 'https://github.com/fujisawagithub/aws_exam.git'
 # deployするブランチ。デフォルトでmainを使用している場合、masterをmainに変更してください。
-set :branch, ENV['BRANCH'] || 'master'
+set :branch, ENV['BRANCH'] || 'main'
 # deploy先のディレクトリ。
 set :deploy_to, '/var/www/achieve'
 # シンボリックリンクをはるフォルダ・ファイル
